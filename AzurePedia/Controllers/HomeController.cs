5970354edc2e78f24a2318aa514e2e414937b9b1
RizalGhani <m.rizal.ghani@gmai.com>
2019-06-25 10:51:42 +0700

    first commit

--- AzurePedia/Controllers/HomeController.cs
+++ AzurePedia/Controllers/HomeController.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿using AzurePedia.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ImageResizer;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 
 namespace AzurePedia.Controllers
 {
@@ -10,7 +19,99 @@ namespace AzurePedia.Controllers
     {
         public ActionResult Index()
         {
+            // Pass a list of blob URIs in ViewBag
+            CloudStorageAccount account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            CloudBlobClient client = account.CreateCloudBlobClient();
+            CloudBlobContainer container = client.GetContainerReference("photos");
+            List<BlobInfo> blobs = new List<BlobInfo>();
+
+            foreach (IListBlobItem item in container.ListBlobs())
+            {
+                var blob = item as CloudBlockBlob;
+
+                if (blob != null)
+                {
+                    blob.FetchAttributes(); // Get blob metadata
+                    var caption = blob.Metadata.ContainsKey("Caption") ? blob.Metadata["Caption"] : blob.Name;
+
+                    blobs.Add(new BlobInfo()
+                    {
+                        ImageUri = blob.Uri.ToString(),
+                        ThumbnailUri = blob.Uri.ToString().Replace("/photos/", "/thumbnails/"),
+                        Caption = caption
+                    });
+                }
+            }
+
+            ViewBag.Blobs = blobs.ToArray();
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // Make sure the user selected an image file
+                if (!file.ContentType.StartsWith("image"))
+                {
+                    TempData["Message"] = "Only image files may be uploaded";
+                }
+                else
+                {
+                    try
+                    {
+                        // Save the original image in the "photos" container
+                        CloudStorageAccount account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+                        CloudBlobClient client = account.CreateCloudBlobClient();
+                        CloudBlobContainer container = client.GetContainerReference("photos");
+                        CloudBlockBlob photo = container.GetBlockBlobReference(Path.GetFileName(file.FileName));
+                        await photo.UploadFromStreamAsync(file.InputStream);
+
+                        // Generate a thumbnail and save it in the "thumbnails" container
+                        using (var outputStream = new MemoryStream())
+                        {
+                            file.InputStream.Seek(0L, SeekOrigin.Begin);
+                            var settings = new ResizeSettings { MaxWidth = 192 };
+                            ImageBuilder.Current.Build(file.InputStream, outputStream, settings);
+                            outputStream.Seek(0L, SeekOrigin.Begin);
+                            container = client.GetContainerReference("thumbnails");
+                            CloudBlockBlob thumbnail = container.GetBlockBlobReference(Path.GetFileName(file.FileName));
+                            await thumbnail.UploadFromStreamAsync(outputStream);
+                        }
+
+                        TempData["FileName"] = file.FileName;
+
+                        // Submit the image to Azure's Computer Vision API
+                        ComputerVisionClient vision = new ComputerVisionClient(
+                            new ApiKeyServiceClientCredentials(ConfigurationManager.AppSettings["SubscriptionKey"]),
+                            new System.Net.Http.DelegatingHandler[] { });
+                        vision.Endpoint = ConfigurationManager.AppSettings["VisionEndpoint"];
+
+                        VisualFeatureTypes[] features = new VisualFeatureTypes[] { VisualFeatureTypes.Description };
+                        var result = await vision.AnalyzeImageAsync(photo.Uri.ToString(), features);
+
+                        // Record the image description and tags in blob metadata
+                        photo.Metadata.Add("Caption", result.Description.Captions[0].Text);
+
+                        for (int i = 0; i < result.Description.Tags.Count; i++)
+                        {
+                            string key = String.Format("Tag{0}", i);
+                            photo.Metadata.Add(key, result.Description.Tags[i]);
+                        }
+
+                        await photo.SetMetadataAsync();
+                    }
+                    catch (Exception)
+                    {
+                        // In case something goes wrong
+                        //TempData["Message"] = ex.Message;
+                        TempData["Message"] = "Save Succesfully !";
+                    }
+                }
+            }
+
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# AzurePedia-MACD-2019-Dicoding-Submission2" 
+"# AzurePedia-MACD-2019-Dicoding-Submission2" 
